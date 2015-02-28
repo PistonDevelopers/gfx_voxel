0f098eccd17c439063cf37e94919f2ba2453bd00
Sven Nilsen <bvssvni@gmail.com>
2015-02-28 15:56:36 +0100

    Reexported array library
    Closes https://github.com/PistonDevelopers/gfx_voxel/issues/40

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,9 @@ extern crate gfx_texture;
 extern crate gfx;
 extern crate image;
 extern crate vecmath;
+extern crate "array" as array_lib;
 
-pub mod array;
+pub use array_lib as array;
+
 pub mod cube;
 pub mod texture;
